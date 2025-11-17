ed785236e1c60c525e2589ea106364ce6f8a9e70
Muhammad Faris <deployments@muhdfaris.com>
2025-11-17 11:37:27 +0800

    fix: intermittent database connection

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,21 +1,7 @@
-import {
-    drizzle,
-    NodePgDatabase
-} from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 import * as schema from '../db/schema';
 
-type TDrizzleInstance = NodePgDatabase<typeof schema> & {
-    $client: Pool;
-};
-
-let drizzleInstance: TDrizzleInstance | null = null;
-
 export function getDrizzle() {
-    if (drizzleInstance !== null) return drizzleInstance;
-
-    drizzleInstance = drizzle(process.env.DATABASE_URL!, { schema });
-
-    return drizzleInstance;
-};
+  return drizzle(process.env.DATABASE_URL!, { schema });
+}
